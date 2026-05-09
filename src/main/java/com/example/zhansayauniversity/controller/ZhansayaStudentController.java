ea3b71db1d674ee5281b7a92f9d512de5a84ad6b
zhans <zhansaya.khasanova.07@mail.ru>
2026-05-09 19:56:35 +0500

    Zhansaya: Implemented RESTful endpoints (POST, PUT, DELETE) with Validation

--- src/main/java/com/example/zhansayauniversity/controller/ZhansayaStudentController.java
+++ src/main/java/com/example/zhansayauniversity/controller/ZhansayaStudentController.java
@@ -1,21 +1,43 @@
 package com.example.zhansayauniversity.controller;
 
+import com.example.zhansayauniversity.dto.ZhansayaStudentDTO;
 import com.example.zhansayauniversity.entity.ZhansayaStudent;
 import com.example.zhansayauniversity.service.ZhansayaStudentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/students")
 public class ZhansayaStudentController {
 
     private final ZhansayaStudentService studentService;
 
-    @GetMapping("/api/students")
+    // 1. GET - Получение всех
+    @GetMapping
     public List<ZhansayaStudent> getAllStudents() {
         return studentService.getAllStudents();
+    }
+
+
+    // 2. POST - Создание студента
+    @PostMapping
+    public String createStudent(@Valid @RequestBody ZhansayaStudentDTO studentDTO) {
+        return "Student " + studentDTO.getFirstName() + " was created!";
+    }
+
+    // 3. PUT - Обновление студента (используем Path Variable {id})
+    @PutMapping("/{id}")
+    public String updateStudent(@PathVariable Long id, @Valid @RequestBody ZhansayaStudentDTO studentDTO) {
+        return "Student with ID " + id + " was updated!";
+    }
+
+    // 4. DELETE - Удаление студента
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        return "Student with ID " + id + " was deleted!";
     }
 }
--- src/main/java/com/example/zhansayauniversity/dto/ZhansayaStudentDTO.java
+++ src/main/java/com/example/zhansayauniversity/dto/ZhansayaStudentDTO.java
@@ -1,11 +1,22 @@
 package com.example.zhansayauniversity.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ZhansayaStudentDTO {
     private Long id;
+
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, message = "First name should have at least 2 characters")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
 }
