6681850ad3dafef1ea61b09ac0c70a50a73d80d6
zhans <zhansaya.khasanova.07@mail.ru>
2026-05-09 16:50:53 +0500

    Zhansaya: Initial project setup and entity creation.

---
+++ src/main/java/com/example/zhansayauniversityapplication/ZhansayaUniversityApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhansayauniversityapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhansayaUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhansayaUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zhansayauniversityapplication/ZhansayaUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhansayauniversityapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhansayaUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
