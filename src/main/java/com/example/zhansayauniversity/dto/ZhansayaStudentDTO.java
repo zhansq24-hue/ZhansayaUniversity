de16ae25165b525331cc7960f39eaa44865b37e1
zhans <zhansaya.khasanova.07@mail.ru>
2026-05-09 19:26:40 +0500

    Zhansaya: Implemented Layered Architecture with StudentDTO and Service mapping

--- src/main/java/com/example/zhansayauniversity/dto/ZhansayaStudentDTO.java
+++ src/main/java/com/example/zhansayauniversity/dto/ZhansayaStudentDTO.java
@@ -3,7 +3,7 @@ package com.example.zhansayauniversity.dto;
 import lombok.Data;
 
 @Data
-class ZhansayaStudentDTO {
+public class ZhansayaStudentDTO {
     private Long id;
     private String firstName;
     private String lastName;
--- src/main/java/com/example/zhansayauniversity/service/ZhansayaStudentService.java
+++ src/main/java/com/example/zhansayauniversity/service/ZhansayaStudentService.java
@@ -1,11 +1,10 @@
 package com.example.zhansayauniversity.service;
 
-import com.example.zhansayauniversity.dto.ZhansayaStudentdto;
 import com.example.zhansayauniversity.entity.ZhansayaStudent;
+import com.example.zhansayauniversity.dto.ZhansayaStudentDTO;
 import com.example.zhansayauniversity.repository.ZhansayaStudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,15 +13,15 @@ import java.util.stream.Collectors;
 public class ZhansayaStudentService {
     private final ZhansayaStudentRepository studentRepository;
 
-    // Метод для получения всех студентов и превращения их в DTO
-    public List<ZhansayaStudentdto> getAllStudents() {
+
+    public List<ZhansayaStudentDTO> getAllStudents() {
         return studentRepository.findAll().stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
-    private ZhansayaStudentdto convertToDTO(ZhansayaStudent student) {
-        ZhansayaStudentdto dto = new ZhansayaStudentdto();
+    private ZhansayaStudentDTO convertToDTO(ZhansayaStudent student) {
+        ZhansayaStudentDTO dto = new ZhansayaStudentDTO();
         dto.setId(student.getId());
         dto.setFirstName(student.getFirstName());
         dto.setLastName(student.getLastName());
