9537a3a06e292ec9fa978cf33199ee607d345038
zhans <zhansaya.khasanova.07@mail.ru>
2026-05-10 15:26:47 +0500

    feat: add JWT security configuration for Swagger UI

--- src/main/java/com/example/zhansayauniversity/ZhansayaUniversityApplication.java
+++ src/main/java/com/example/zhansayauniversity/ZhansayaUniversityApplication.java
@@ -1,8 +1,22 @@
 package com.example.zhansayauniversity;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(
+        info = @Info(title = "University Management API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @SpringBootApplication
 public class ZhansayaUniversityApplication {
     public static void main(String[] args) {
